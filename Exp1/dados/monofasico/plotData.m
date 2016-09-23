e95776345a3452c50a6152a16636feea890b3f70
Dello <daniel.dello@clubedaentrega.com.br>
2016-09-23 09:44:43 -0300

    Finished exp1 report

--- Exp1/dados/monofasico/plotData.m
+++ Exp1/dados/monofasico/plotData.m
@@ -28,4 +28,25 @@ title('Tensão fornecida pela fonte')
 ylabel('Tensão [V]')
 xlabel('Tempo [s]')
 print('mono_s', '-deps')
+%%
+windowSize = 10;
+b = (1/windowSize)*ones(1,windowSize);
+a = 1;
+
+tensao_deslocada = interp1(fonte.data(:,1) + (6e-4), abs(fonte.data(:,2)),carga.data(:,4));
+queda_diodo = filter(b,a,tensao_deslocada) - filter(b,a,carga.data(:,3));
+figure,
+plot(carga.data(:,4), filter(b,a,tensao_deslocada), carga.data(:,4), filter(b,a,carga.data(:,3)))
+ylabel('Tensão [V]')
+xlabel('Tempo [s]')
+legend('Fonte', 'Carga')
+print('abs', '-dpdf')
+figure,
+plot(carga.data(:,4), queda_diodo)
+title('Queda de tensão no diodo')
+ylabel('Tensão [V]')
+xlabel('Tempo [s]')
+print('diff', '-dpdf')
+
+nanmean(queda_diodo)
 !epsfixer.sh
