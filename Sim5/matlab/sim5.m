fa6dc12801f9cd66650f6d94ded57c2b791def61
Dello <daniel.dello@clubedaentrega.com.br>
2016-11-08 15:20:30 -0200

    Melhorias sim5

--- Sim5/matlab/sim5.m
+++ Sim5/matlab/sim5.m
@@ -34,7 +34,7 @@ results.motor.Mechanical.get('Electromagnetic torque Te (N*m)').plot(),
 title('Tem')
 ylabel('Torque (N*m)')
 xlabel('Time (seconds)')
-legend('Tref', 'Tem')
+legend('Load Torque', 'Tem')
 print('tem', '-depsc')
 fig = figure;
 plot(results.wm.Time, 9.54929659642538*results.wm.Data, results.wref.Time, results.wref.Data, results.ctrl.get('Speed reference (rpm)').Time, results.ctrl.get('Speed reference (rpm)').Data)
@@ -50,7 +50,12 @@ title('Torque - Slip')
 xlabel('slip')
 ylabel('Electromagnetic torque Te (N*m)')
 print('ts', '-depsc')
-
+figure,
+results.slip.plot()
+title('Slip')
+xlabel('Time (seconds)')
+ylabel('slip')
+print('slip', '-depsc')
 %% Sim 2
 Vsour = 220;
 Fsour = 60;
@@ -75,10 +80,10 @@ results.motor.Mechanical.get('Electromagnetic torque Te (N*m)').plot(),
 title('Tem')
 ylabel('Torque (N*m)')
 xlabel('Time (seconds)')
-legend('Tref', 'Tem')
+legend('Load Torque', 'Tem')
 print('tem2', '-depsc')
 fig = figure;
-plot(results.wm.Time, 9.54929659642538*results.wm.Data, results.wref.Time, results.wref.Data, results.ctrl.get('Speed reference (rpm)').Time, results.ctrl.get('Speed reference (rpm)').Data))
+plot(results.wm.Time, 9.54929659642538*results.wm.Data, results.wref.Time, results.wref.Data, results.ctrl.get('Speed reference (rpm)').Time, results.ctrl.get('Speed reference (rpm)').Data)
 title('wm')
 ylabel('Speed (rpm)')
 xlabel('Time (seconds)')
@@ -91,3 +96,11 @@ title('Torque - Slip')
 xlabel('slip')
 ylabel('Electromagnetic torque Te (N*m)')
 print('ts2', '-depsc')
+figure,
+results.slip.plot()
+title('Slip')
+xlabel('Time (seconds)')
+ylabel('slip')
+print('slip2', '-depsc')
+%% Fix eps
+!epsfixer.sh
