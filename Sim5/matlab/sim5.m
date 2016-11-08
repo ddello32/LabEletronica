1e1344761ad68341040eca7b12d3bfc70a12fe96
Dello <daniel.dello@clubedaentrega.com.br>
2016-11-08 11:48:43 -0200

    Comecei sim 5

--- Sim5/matlab/sim5.m
+++ Sim5/matlab/sim5.m
@@ -12,7 +12,7 @@ Llr = 0.013;
 Lm = 0.296;
 J = 0.0654;
 F = 0.005;
-%%
+%% Sim 1
 Vsour = 220;
 Fsour = 60;
 wref = 1500;
@@ -20,7 +20,6 @@ ti = 1;
 tf  = 5.5;
 tt = 6;
 stime = 9;
-%% Simular
 sim('ac2')
 
 %% Plots
@@ -38,17 +37,57 @@ xlabel('Time (seconds)')
 legend('Tref', 'Tem')
 print('tem', '-depsc')
 fig = figure;
-plot(results.wm.Time, 9.54929659642538*results.wm.Data, results.wref.Time, results.wref.Data)
+plot(results.wm.Time, 9.54929659642538*results.wm.Data, results.wref.Time, results.wref.Data, results.ctrl.get('Speed reference (rpm)').Time, results.ctrl.get('Speed reference (rpm)').Data)
 title('wm')
-ylabel('Speed (rad/s)')
+ylabel('Speed (rpm)')
 xlabel('Time (seconds)')
-legend('wm', 'wref')
+legend('wm', 'wref', 'controler wref')
 title('wm')
 print('wm', '-depsc')
 figure,
-slip = (results.ctrl.get('Frequency reference (Hz)')*2*pi - results.motor.Mechanical.get('Rotor speed (wm)'))./(results.ctrl.get('Frequency reference (Hz)')*2*pi);
-plot(slip.Data, results.motor.Mechanical.get('Electromagnetic torque Te (N*m)').Data)
+plot(results.slip.Data, results.motor.Mechanical.get('Electromagnetic torque Te (N*m)').Data)
 title('Torque - Slip')
 xlabel('slip')
 ylabel('Electromagnetic torque Te (N*m)')
 print('ts', '-depsc')
+
+%% Sim 2
+Vsour = 220;
+Fsour = 60;
+wref = 1500;
+ti = 0;
+tf  = 1;
+tt = 6;
+t1 = 1;
+t2 = 4;
+stime = 9;
+sim('ac3')
+
+%% Plots
+fig = figure;
+results.motor.get('Stator measurements').get('Stator current is_a (A)').plot()
+title('Ias')
+print('ias2', '-depsc')
+fig = figure;
+results.tref.plot('g')
+hold,
+results.motor.Mechanical.get('Electromagnetic torque Te (N*m)').plot(),
+title('Tem')
+ylabel('Torque (N*m)')
+xlabel('Time (seconds)')
+legend('Tref', 'Tem')
+print('tem2', '-depsc')
+fig = figure;
+plot(results.wm.Time, 9.54929659642538*results.wm.Data, results.wref.Time, results.wref.Data, results.ctrl.get('Speed reference (rpm)').Time, results.ctrl.get('Speed reference (rpm)').Data))
+title('wm')
+ylabel('Speed (rpm)')
+xlabel('Time (seconds)')
+legend('wm', 'wref', 'controler wref')
+title('wm')
+print('wm2', '-depsc')
+figure,
+plot(results.slip.Data, results.motor.Mechanical.get('Electromagnetic torque Te (N*m)').Data)
+title('Torque - Slip')
+xlabel('slip')
+ylabel('Electromagnetic torque Te (N*m)')
+print('ts2', '-depsc')
